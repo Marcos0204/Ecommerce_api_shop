dd8688c55e3cf033f2c037031b21d964688b242a
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-11-11 00:10:30 +0100

    update-crud

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const dotenv = require('dotenv')
 const userRoute = require('./routes/user')
 const authRouter = require('./routes/auth')
 const productRouter = require('./routes/product')
+const cartRouter = require('./routes/cart')
+const ordertRouter = require('./routes/order')
 
 dotenv.config()
 
@@ -14,7 +16,9 @@ mongoose.connect(process.env.MONGO_URL).then(() => console.log('DB_conectada Suc
 app.use(express.json())
 app.use('/api/auth', authRouter)
 app.use('/api/users', userRoute)
-app.use('/api/products', productRouter)
+app.use('/api/carts', productRouter)
+app.use('/api/products', cartRouter)
+app.use('/api/orders', ordertRouter)
 
 app.listen(process.env.PORT || 5000, () =>{
     console.log('app liste, active')

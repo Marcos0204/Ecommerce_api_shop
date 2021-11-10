d57da1826070446cc4a9d174fd001f20b87f863e
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-11-10 18:41:59 +0100

    updatelogin-jwt

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const User = require('../models/User')
 const CryptoJS = require('crypto-js')
+const jwt = require("jsonwebtoken");
 
 ///regiter
 router.post("/register", async (req, res) => {
@@ -37,9 +38,19 @@ router.post("/login", async (req, res)=>{
     
       const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
       OriginalPassword !== req.body.password && res.status(401).json('wrong credentials!')
+        
+      const accessToken = jwt.sign(
+        {
+            id: user._id,
+            isAdmin: user.isAdmin,
+        },
+        process.env.JWT_SEC,
+            {expiresIn:"3d"}
+        );
+      
       const { password, ...others } = user._doc
       
-    res.status(200).json(others)
+    res.status(200).json({...others, accessToken})
 
   } catch (error) {
     res.status(500).json(error)

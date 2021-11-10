e81dd281feff60e6d1533acc469d1479e6880b01
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-11-10 12:18:12 +0100

    update-config-inital

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
+const dotenv = require('dotenv')
 
-mongoose.connect('mongodb+srv://marcos:marcospasswor@cluster0.kjm0o.mongodb.net/MERNTASKS?retryWrites=true&w=majority').then(() => console.log('DB_conectada')).catch((err) =>{
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URL).then(() => console.log('DB_conectada SuccessFull!!!')).catch((err) =>{
     console.log(err)
 })
 
-app.listen(5000, () =>{
+app.listen(process.env.PORT ||5000, () =>{
     console.log('app liste, active')
 })
